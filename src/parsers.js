0b8ea06456eb18f1fa5542902db1c1630a946b51
TimFix <110151346+TimFix@users.noreply.github.com>
2023-08-17 16:00:23 +0300

    4

--- src/parsers.js
+++ src/parsers.js
@@ -4,6 +4,9 @@ export default (data, extension) => {
   switch (extension) {
     case 'json':
       return JSON.parse(data);
+    case 'yaml':
+    case 'yml' :
+      return load(data);
     default:
       throw new Error(`Unknown extension ${extension}`);
   }
