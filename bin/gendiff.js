d0d203fe3f2954030d77e998a9a85dd25926e17f
TimFix <110151346+TimFix@users.noreply.github.com>
2023-08-17 09:05:22 +0300

    Update gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,7 +7,7 @@ const program = new Command();
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .argument('<filepath1> <filepath2>')
+  .arguments('<filepath1> <filepath2>')
   .option('-v, --version', 'output the version number')
   .option('-f, --format <type>', 'output format', 'stylish')
   .action((filepath1, filepath2) => {
