cb75242748706d1af63378cd8246dc6b283ad491
TimFix <110151346+TimFix@users.noreply.github.com>
2023-08-18 14:46:54 +0300

    Add files via upload

--- src/buildTree.js
+++ src/buildTree.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 
-export default (data1, data2) => {
+const buildTree = (data1, data2) => {
   const keys1 = _.keys(data1);
   const keys2 = _.keys(data2);
   const sortedKeys = _.sortBy(_.union(keys1, keys2));
@@ -12,6 +12,9 @@ export default (data1, data2) => {
     if (!_.has(data2, key)) {
       return [key, { type: 'not', value: data1[key] }];
     }
+    if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
+      return [key, { type: 'nested', children: buildTree(data1[key], data2[key]) }];
+    }
     if (data1[key] !== data2[key]) {
       return [key, { type: 'changed', value1: data1[key], value2: data2[key] }];
     }
@@ -19,3 +22,5 @@ export default (data1, data2) => {
   });
   return _.fromPairs(tree);
 };
+
+export default buildTree;
--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,24 +1,34 @@
 import _ from 'lodash';
 
+const getIndent = (depth, space = ' ', spaceLength = 4) => space.repeat(depth * spaceLength - 2);
+const getBracketIndent = (depth, space = ' ', spaceLength = 4) => space.repeat((depth - 1) * spaceLength);
+
 export default (tree) => {
-    if (!_.isPlainObject(tree)) {
-      return `${tree}`;
+  const iter = (currentValue, depth = 1) => {
+    if (!_.isPlainObject(currentValue)) {
+      return `${currentValue}`;
     }
+
     const buildedResult = Object
-      .entries(tree)
+      .entries(currentValue)
       .map(([key, info]) => {
         switch (info.type) {
-          case 'new': 
-            return `  + ${key}: ${info.value}`
+          case 'new':
+            return `${getIndent(depth)}+ ${key}: ${iter(info.value, depth + 1)}`;
           case 'not':
-            return `  - ${key}: ${info.value}`
+            return `${getIndent(depth)}- ${key}: ${iter(info.value, depth + 1)}`;
           case 'equally':
-            return `    ${key}: ${info.value}`
+            return `${getIndent(depth)}  ${key}: ${iter(info.value, depth + 1)}`;
+          case 'nested':
+            return `${getIndent(depth)}  ${key}: ${iter(info.children, depth + 1)}`;
           case 'changed':
-            return `  - ${key}: ${info.value}\n  + ${key}: ${info.value}`;
+            return `${getIndent(depth)}- ${key}: ${iter(info.value1, depth + 1)}\n${getIndent(depth)}+ ${key}: ${iter(info.value2, depth + 1)}`;
           default:
-            return `    ${key}: ${info.value}`;
+            return `${getIndent(depth)}  ${key}: ${iter(info.value || info, depth + 1)}`;
         }
       });
-    return ['{', ...buildedResult, ` }`].join('\n');
+    return ['{', ...buildedResult, `${getBracketIndent(depth)}}`].join('\n');
+  };
+
+  return iter(tree);
 };
